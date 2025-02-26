ba8902444efac027d59f23879976b5441c4bd2d9
Zidanbz <zidanbz03@gmail.com>
2025-02-26 22:26:00 +0800

    update

--- src/layout/dashboardLayout.jsx
+++ src/layout/dashboardLayout.jsx
@@ -1,10 +1,46 @@
+
+
+import Navbar from "../components/landingPage/Navbar";
 import SideBar from "../components/Sidebar";
 
 const DashboardLayout = () => {
   return (
-    <div>
-      <SideBar />
-    </div>
+<div className="bg-gray-100">
+              
+            <div className="flex">
+                <SideBar />
+                <main className="flex-1 pt-0 ">
+                      <Navbar/>
+                    <div className="flex justify-between items-center mb-6 pl-6">
+                        <h2 className="text-xl font-bold">My Activity</h2>
+                    </div>
+                    <div className="grid grid-flow-col gap-6 overflow-x-auto pl-6">
+                        {[25, 50, 75].map((progress, index) => (
+                            <div 
+                                key={index} 
+                                className="bg-white p-4 rounded-lg shadow-lg w-[300px] cursor-pointer hover:shadow-xl transition duration-200"
+                                onClick={() => navigate("/ProgressTrainee")}
+                            >
+                                <img
+                                    src="/src/assets/Icon/Maskot.png"
+                                    alt={`Course ${index + 1}`}
+                                    className="w-full h-[200px] object-cover rounded-t-lg"
+                                />
+                                <div className="p-4">
+                                    <h3 className="text-lg font-bold">Pemrograman Web</h3>
+                                    <p className="mt-2 text-gray-600">Nama Trainee: Mahmud</p>
+                                    <p className="mt-2 text-gray-600">{index + 1} / 4 Lessons</p>
+                                    <div className="mt-2 bg-gray-200 rounded-full h-2">
+                                        <div className="bg-blue-500 h-2 rounded-full" style={{ width: `${progress}%` }}></div>
+                                    </div>
+                                    <p className="mt-2 text-right text-gray-600">{progress}%</p>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </main>
+            </div>
+        </div>
   );
 };
 
--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_URL = "https://widgets-catb7yz54a-uc.a.run.app";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+const API_URL = API_BASE_URL;
 
 export const registerMentor = async (formData) => {
   try {
